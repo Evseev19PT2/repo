8670d3492de269082a4fa4ae344fccfabe8795d4
Evseev19PT2 <66921130+Evseev19PT2@users.noreply.github.com>
2026-06-24 19:59:14 +0300

    Add files via upload

---
+++ tools/schema/violations.mjs
@@ -0,0 +1,87 @@
+import ruleSummary from "./rule-summary.mjs";
+import violationType from "./violation-type.mjs";
+import miniDependency from "./mini-dependency-type.mjs";
+import dependencyType from "./dependency-type.mjs";
+
+export default {
+  definitions: {
+    ViolationsType: {
+      type: "array",
+      description:
+        "A list of violations found in the dependencies. The dependencies themselves " +
+        "also contain this information, this summary is here for convenience.",
+      items: { $ref: "#/definitions/ViolationType" },
+    },
+    ViolationType: {
+      type: "object",
+      required: ["from", "to", "rule"],
+      additionalProperties: false,
+      properties: {
+        from: {
+          type: "string",
+        },
+        to: {
+          type: "string",
+          description:
+            "The (resolved) 'to' part of the dependency this violation is about",
+        },
+        unresolvedTo: {
+          type: "string",
+          description:
+            "The (unresolved) 'to' part of the dependency this violation is about",
+        },
+        dependencyTypes: {
+          type: "array",
+          items: { $ref: "#/definitions/DependencyTypeType" },
+        },
+        type: { $ref: "#/definitions/ViolationTypeType" },
+        rule: { $ref: "#/definitions/RuleSummaryType" },
+        cycle: {
+          type: "array",
+          items: { $ref: "#/definitions/MiniDependency" },
+          description:
+            "The circular path if the violation is about circularity",
+        },
+        via: {
+          type: "array",
+          items: { $ref: "#/definitions/MiniDependency" },
+          description:
+            "The path from the from to the to if the violation is transitive",
+        },
+        metrics: {
+          type: "object",
+          required: ["from", "to"],
+          additionalProperties: false,
+          properties: {
+            from: {
+              type: "object",
+              required: ["instability"],
+              additionalProperties: false,
+              properties: {
+                instability: { type: "number" },
+              },
+            },
+            to: {
+              type: "object",
+              required: ["instability"],
+              additionalProperties: false,
+              properties: {
+                instability: { type: "number" },
+              },
+            },
+          },
+        },
+        comment: {
+          type: "string",
+          description:
+            "Free format text you can e.g. use to explain why this violation " +
+            "can be ignored or is quarantined (only used in _known-violations_) ",
+        },
+      },
+    },
+    ...ruleSummary.definitions,
+    ...violationType.definitions,
+    ...miniDependency.definitions,
+    ...dependencyType.definitions,
+  },
+};
